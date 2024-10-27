96b93f69a54a7d2bf0d08b89ae88f36a80aa3662
rathnayr <rathnayr@metropolia.fi>
2024-10-27 18:31:25 +0530

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,1 +1,15 @@
+from car import Car
 
+car = Car ("ABC-123",142)
+
+car.accelerate(30)
+car.accelerate(70)
+car.accelerate(50)
+
+print("Car Details After Increasing Speed:")
+car.print_car_details()
+
+car.accelerate(-200)
+
+print("Car Details After Emergency Braking:")
+car.print_car_details()
